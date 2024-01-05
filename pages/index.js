0c26158e283d6f29c3da6eeab2e310f476f20f32
amber7117 <90747456+amber7117@users.noreply.github.com>
2024-01-06 07:54:02 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import TopHeader from "../components/Layouts/TopHeader";
 import Navbar from "../components/Layouts/Navbar";
-import PageBanner from "../components/Common/PageBanner";
+import MainBanner from '../components/HomeOne/MainBanner';
 import BlogGridFullWidth from "../components/Blog/BlogGridFullWidth";
 import FacilitySlider from "../components/Common/FacilitySlider";
 import InstagramFeed from "../components/Common/InstagramFeed";
 import Footer from "../components/Layouts/Footer";
-
-const Blog4 = ({ user, store }) => {
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
+const Index = ({user,products,store}) => {
   return (
     <>
       <TopHeader user={user} />
       <Navbar user={user} store={store} />
+       <MainBanner />
       <BlogGridFullWidth />
       <FacilitySlider />
       <InstagramFeed />
@@ -20,4 +22,27 @@ const Blog4 = ({ user, store }) => {
   );
 };
 
-export default Blog4;
+
+export async function getServerSideProps(ctx) {
+  const page = ctx.query.page ? ctx.query.page : '1';
+
+  const payload = {
+    params: {
+      page,
+      limit: 100,
+    }
+  };
+
+  const url = `${baseUrl}/api/v1/products`;
+
+  const response = await axios.get(url, payload);
+  const { products, totalPages } = response.data;
+
+  return {
+    props: {
+      products,
+      totalPages,
+    },
+  };
+}
+export default Index;
