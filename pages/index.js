cc1578c20817805da7ecc482c4688079d97618f7
amber7117 <90747456+amber7117@users.noreply.github.com>
2024-01-06 15:00:45 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import TopHeader from "../components/Layouts/TopHeader";
 import Navbar from "../components/Layouts/Navbar";
 import GalleryThreeGridFullWidth from "../components/Gallery/GalleryThreeGridFullWidth";
-import BlogDetailsTwo from "../components/BlogDetails/BlogDetailsTwo";
 import Footer from "../components/Layouts/Footer2";
 
 const Index = ({ user, store }) => {
@@ -9,7 +8,6 @@ const Index = ({ user, store }) => {
     <>
       <TopHeader user={user} />
       <Navbar user={user} store={store} />
-      <BlogDetailsTwo />
       <GalleryThreeGridFullWidth />
       <Footer />
     </>
