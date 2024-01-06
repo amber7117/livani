27347f5044da2e99c8bdb164e93b63d7c08f5bee
amber7117 <90747456+amber7117@users.noreply.github.com>
2024-01-06 13:20:04 +0800

    Update BlogDetailsTwo.js

--- components/BlogDetails/BlogDetailsTwo.js
+++ components/BlogDetails/BlogDetailsTwo.js
@@ -3,8 +3,6 @@ import Link from 'next/link';
 import CommentsList from './CommentsList';
 import BlogSidebar from '../Blog/BlogSidebar';
 
-
-
 class BlogDetailsTwo extends Component {
   render() {
     return (
@@ -16,37 +14,7 @@ class BlogDetailsTwo extends Component {
                 <div className='article-video'>
                   <iframe src='https://www.youtube.com/embed/bk7McNUjWgw'></iframe>
                 </div>
-                <div className='article-video'>
-	    		        <iframe src="https://www.pornhub.com/embed/64bab88ec37ef"></iframe>
-	    	        </div>
-             	  <div className='article-video'>
-      			      <iframe src="https://www.pornhub.com/embed/64bab88ec37ef"></iframe>
-	    	        </div>
-	              <div className='article-video'>
-                  <iframe src="https://www.pornhub.com/embed/64bab88ec37ef"></iframe>
-              </div>
-	            <div className='article-video'>
-      		         <iframe src="https://www.pornhub.com/embed/65781ff4aa1f0" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-             </div>
-	           <div className='article-video'>
-      		      <iframe src="https://www.pornhub.com/embed/64da8334365f7" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-      	     </div>
-	          <div className='article-video'>
-	              <iframe src="https://www.pornhub.com/embed/6478b624850bf" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-	          </div>
-            <div className='article-video'>
-	    	        <iframe src="https://www.pornhub.com/embed/657b20c45d388" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-	          </div>
-	          <div className='article-video'>
-	    	        <iframe src="https://www.pornhub.com/embed/65687ea9e3e96" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-	          </div>
-            <div className='article-video'>
-                <iframe src="https://www.pornhub.com/embed/ph6149f33dcbe3a" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-      	    </div>
-	          <div className='article-video'>
-		            <iframe src="https://www.pornhub.com/embed/65769e14ee10c" frameborder="0" width="560" height="315" scrolling="no" allowfullscreen></iframe>
-	          </div>
-				</div>
+
                 <div className='article-content'>
                   <div className='entry-meta'>
                     <ul>
@@ -252,10 +220,17 @@ class BlogDetailsTwo extends Component {
 
                         <div className='author-profile-raque-profile'>
                           <Link href='#'>
+                            <a className='d-inline-block'>
+                              View profile on Livani
+                            </a>
                           </Link>
                         </div>
                       </div>
                     </div>
+                    <p>
+                      Orwig is a celebrated photographer, author, and writer who
+                      brings passion to everything he does.
+                    </p>
                   </div>
                 </div>
 
@@ -263,8 +238,12 @@ class BlogDetailsTwo extends Component {
                   <div className='prev-link-wrapper'>
                     <div className='info-prev-link-wrapper'>
                       <Link href='#'>
-                    
+                        <a>
                           <span className='image-prev'>
+                            <img
+                              src='../images/blog/blog5.jpg'
+                              alt='image'
+                            />
                             <span className='post-nav-title'>Prev</span>
                           </span>
 
@@ -287,7 +266,7 @@ class BlogDetailsTwo extends Component {
                   <div className='next-link-wrapper'>
                     <div className='info-next-link-wrapper'>
                       <Link href='#'>
-       
+                        <a>
                           <span className='next-link-info-wrapper'>
                             <span className='next-title'>
                               Building eCommerce wave: Social media shopping
@@ -306,21 +285,25 @@ class BlogDetailsTwo extends Component {
                             />
                             <span className='post-nav-title'>Next</span>
                           </span>
-                 
+                        </a>
                       </Link>
                     </div>
                   </div>
                 </div>
 
-  
-
+                {/* Comments List */}
+                <CommentsList />
               </div>
             </div>
 
-       
+            <div className='col-lg-4 col-md-12'>
+              {/* Blog Sidebar */}
+              <BlogSidebar />
+            </div>
+          </div>
         </div>
       </section>
-    )
+    );
   }
 }
 
