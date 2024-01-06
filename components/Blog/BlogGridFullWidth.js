538faf036011368139d1f53e183f92ab4b2c1359
amber7117 <90747456+amber7117@users.noreply.github.com>
2024-01-06 08:08:08 +0800

    Update BlogGridFullWidth.js

--- components/Blog/BlogGridFullWidth.js
+++ components/Blog/BlogGridFullWidth.js
@@ -5,7 +5,7 @@ class BlogGridFullWidth extends Component {
   render() {
     return (
       <section className='blog-area ptb-100'>
-        <div className='container-fluid'>
+ <div className='container-fluid'>
           <div className='row'>
             <div className='col-lg-3 col-md-6'>
               <div className='single-blog-post'>
@@ -254,6 +254,255 @@ class BlogGridFullWidth extends Component {
                 </div>
               </div>
             </div>
+
+ <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog1.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Ideas</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>The #1 eCommerce blog to grow your business</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog2.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Advice</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>Latest ecommerce trend: The rise of shoppable posts</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog3.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Social</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>Building eCommerce wave: Social media shopping</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog4.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Boy</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>The best eCommerce blogs for online retailers</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+			
+			 <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog1.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Ideas</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>The #1 eCommerce blog to grow your business</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog2.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Advice</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>Latest ecommerce trend: The rise of shoppable posts</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog3.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Social</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>Building eCommerce wave: Social media shopping</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-lg-3 col-md-6'>
+              <div className='single-blog-post'>
+                <div className='post-image'>
+                  <Link href='/single-blog-1'>
+                    <a>
+                      <img
+                        src='../images/blog/blog4.jpg'
+                        alt='image'
+                      />
+                    </a>
+                  </Link>
+                  <div className='date'>
+                    <span>January 29, 2020</span>
+                  </div>
+                </div>
+
+                <div className='post-content'>
+                  <span className='category'>Boy</span>
+                  <h3>
+                    <Link href='/single-blog-1'>
+                      <a>The best eCommerce blogs for online retailers</a>
+                    </Link>
+                  </h3>
+
+                  <Link href='/single-blog-1'>
+                    <a className='details-btn'>Read Story</a>
+                  </Link>
+                </div>
+              </div>
+            </div>
+
 
             <div className='col-lg-12 col-md-12 col-sm-12'>
               <div className='pagination-area text-center'>
