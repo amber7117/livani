3326decdf5fa2b3bcac138e274548e659f1dedec
amber7117 <90747456+amber7117@users.noreply.github.com>
2024-01-10 09:08:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Navbar from "../components/Layouts/Navbar";
 import BlogPost from '../components/HomeElectronics/BlogPost';
 import BlogDetailsThree from "../components/BlogDetails/BlogDetailsThree";
 import Footer from "../components/Layouts/Footer2";
+import RecentProducts from '../components/Shared/RecentProducts';
 
 const Index = ({ user, store }) => {
   return (
     <>
       <TopHeader user={user} />
       <Navbar user={user} store={store} />
+      <RecentProducts products={products.slice(26,32)} />
       <BlogPost />
  
       <Footer />
